fc411192e560522dafcac17a06c59276e3bd451f
gauri691 <gaurijagadeesh.b23cs1228@mbcet.ac.in>
2024-12-02 11:56:01 +0530

    hotel-rates

--- hotels.java
+++ hotels.java
@@ -1,38 +1,35 @@
 import java.util.Scanner;
-public class hotels {
+public class Hotels {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of nights:");
         int night = sc.nextInt();
         System.out.println("Enter the room type:\n 1.STANDARD \n 2. DELUXE \n 3.SUITE ");
-        String room_type = sc.next().toLowerCase();
-        double totalAmount;
-        double result = switch (room_type) {
-            case "standard" -> {
-                totalAmount = 500 * night;
-                if (night >= 5) {
-                    totalAmount -= totalAmount * 0.15;
-                    totalAmount += totalAmount * 0.10;
-                }
-                yield totalAmount;
-            }
-            case "deluxe" -> {
-                totalAmount = 800 * night;
-                if (night >= 5) {
-                    totalAmount -= totalAmount * 0.15;
-                    totalAmount += totalAmount * 0.10;
-                }
-                yield totalAmount;
-            }
-            case "suite" -> {
-                totalAmount = 1000 * night;
-                if (night >= 5) {
-                    totalAmount -= totalAmount * 0.15;
-                    totalAmount += totalAmount * 0.10;
-                }
-                yield totalAmount;
+        String roomType = sc.next().toLowerCase();
+        double totalAmount=0.0;
+        double roomCost;
+        double base = switch (roomType) {
+            case "standard" -> 500;
+
+            case "deluxe" -> 800;
+
+            case "suite" -> 1000;
+
+            default -> {
+                System.out.println("Invalid room type.");
+                yield 0.0;
             }
         };
+        if (night >= 5) {
+            totalAmount=base*night;
+            totalAmount -= totalAmount * 0.15;
+            totalAmount += totalAmount * 0.10;
+            System.out.println("Total Amount:"+totalAmount);
+        }
+        else{
+            totalAmount=base*night;
+            System.out.println("Total Amount:"+totalAmount);
+        }
     }
 }
 
