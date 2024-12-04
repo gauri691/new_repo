95a3f1da192d9ca6a05bae98c293c4d96a10a63e
gauri691 <gaurijagadeesh.b23cs1228@mbcet.ac.in>
2024-12-04 11:38:24 +0530

    string-duplicate

--- Main-projects/src/strings.java
+++ Main-projects/src/strings.java
@@ -1,5 +1,21 @@
-public class strings{
-    public static void main(String[]args){
-        
+import java.util.HashSet;
+import java.util.Scanner;
+public class strings
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a string:");
+            String sentence = sc.nextLine();
+            sentence =sentence.toLowerCase();
+            String[] words = sentence.split(" ");
+            HashSet hashset=new HashSet();
+        for(String i:words)
+        {
+                if (!hashset.add(i))
+                {
+                    System.out.println("Duplicate word is:"+i);
+                }
+            }
+        }
     }
-}
