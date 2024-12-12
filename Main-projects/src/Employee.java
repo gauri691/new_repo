d1718185bf909b6a6f01b318951b0b2f532db402
gauri691 <gaurijagadeesh.b23cs1228@mbcet.ac.in>
2024-12-12 14:53:55 +0530

    employee-details

--- Main-projects/src/Employee.java
+++ Main-projects/src/Employee.java
@@ -11,10 +11,11 @@ public class Employee {
             return departmentName;
         }
     }
+
     public class Address {
-        private String street;
-        private String houseNO;
-        private String pincode;
+        String street;
+        String houseNO;
+        String pincode;
 
         public Address(String street, String houseNO, String pincode) {
             this.street = street;
@@ -22,25 +23,28 @@ public class Employee {
             this.pincode = pincode;
         }
     }
-    private int id;
-    private String name;
-    private Department department;
-    private Address address;
 
-    public  Employee(int id,String name,String departmentName,String street,String houseNO,String pincode){
-        this.id=id;
-        this.name=name;
-        this.department=new Department(departmentName);
-        this.address=new Address(street, houseNO, pincode);
+    int id;
+    String name;
+    Department department;
+    Address address;
+
+    public Employee(int id, String name, String departmentName, String street, String houseNO, String pincode) {
+        this.id = id;
+        this.name = name;
+        this.department = new Department(departmentName);
+        this.address = new Address(street, houseNO, pincode);
     }
-    public void displayDetails(){
-        System.out.println("id:"+id);
-        System.out.println("name:"+name);
-        System.out.println("department:"+department.getDepartmentName());
-        System.out.println("Street:"+address.street+"  house number:"+address.houseNO+"  pincode:"+address.pincode);
+
+    public void displayDetails() {
+        System.out.println("id:" + id);
+        System.out.println("name:" + name);
+        System.out.println("department:" + department.getDepartmentName());
+        System.out.println("Street:" + address.street + "  house number:" + address.houseNO + "  pincode:" + address.pincode);
     }
-}
-public void main(String[] args){
-    Employee emp=new Employee(101,"Gauri","CS","123 Elm St","4802","695029");
-    emp.displayDetails();
+
+    public static void main(String[] args) {
+        Employee emp = new Employee(101, "Gauri", "CS", "123 Elm St", "4802", "695029");
+        emp.displayDetails();
+    }
 }
