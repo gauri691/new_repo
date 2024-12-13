c474056bb1b82088210ea615ed81676198cc6ee2
gauri691 <gaurijagadeesh.b23cs1228@mbcet.ac.in>
2024-12-13 13:11:09 +0530

    Employee-exception-handling

--- Main-projects/src/EmployeException.java
+++ Main-projects/src/EmployeException.java
@@ -4,7 +4,7 @@ public class EmployeException {
         Scanner sc = new Scanner(System.in);
         String[] name = new String[100];
         double[] salary = new double[100];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             try {
                 System.out.println("Enter name of " + (i + 1) + "th employee");
                 String employeName = sc.nextLine();
@@ -16,15 +16,22 @@ public class EmployeException {
                 double doubleSalary = Double.parseDouble(employeSalary);
 
                 if (doubleSalary < 0) {
-                    throw new NumberFormatException("negative number is not allowed.");
+                    throw new IllegalArgumentException("negative number is not allowed.");
                 }
                 name[i] = employeName;
                 salary[i] = doubleSalary;
-            } catch (Exception e) {
-                System.out.println("Error message" + e);
+            }
+            catch(NumberFormatException e) {
+                System.out.println("ERROR: "+e);
+            }
+            catch(IllegalArgumentException e){
+                System.out.println("ERROR: "+e);
+            }
+             catch(Exception e) {
+                System.out.println("ERROR" + e);
             }
         }
-            for (int j = 0; j < 10; j++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.println(name[j] + " got a salary " + salary[j]);
             }
         }
