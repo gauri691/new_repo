f620265863ccebb771e81337c527d699bddc9c3f
gauri691 <gaurijagadeesh.b23cs1228@mbcet.ac.in>
2024-12-13 12:33:25 +0530

    Employee-exception-handling

---
+++ Main-projects/src/EmployeException.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class EmployeException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] name = new String[100];
+        double[] salary = new double[100];
+        for (int i = 0; i < 10; i++) {
+            try {
+                System.out.println("Enter name of " + (i + 1) + "th employee");
+                String employeName = sc.nextLine();
+                if (employeName == null ||employeName.isEmpty() ) {
+                    throw new IllegalArgumentException("Name cannot be null or cannot be empty");
+                }
+                System.out.println("Enter salary of " + (i + 1) + "th employee");
+                String employeSalary = sc.nextLine();
+                double doubleSalary = Double.parseDouble(employeSalary);
+
+                if (doubleSalary < 0) {
+                    throw new NumberFormatException("negative number is not allowed.");
+                }
+                name[i] = employeName;
+                salary[i] = doubleSalary;
+            } catch (Exception e) {
+                System.out.println("Error message" + e);
+            }
+        }
+            for (int j = 0; j < 10; j++) {
+                System.out.println(name[j] + " got a salary " + salary[j]);
+            }
+        }
+    }
