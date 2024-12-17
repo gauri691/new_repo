aaeb9a9dd552fb2873e88d5aef1392f9db89044f
gauri691 <gaurijagadeesh.b23cs1228@mbcet.ac.in>
2024-12-17 11:35:11 +0530

    Library-Managemet-collection

--- Main-projects/src/LibraryProject/Member.java
+++ Main-projects/src/LibraryProject/Member.java
@@ -38,7 +38,7 @@ import LibraryProject.*;
                                     System.out.println("The book has been issued.");
                                     book.setBookAvailability(false);
                                     int borrowCopies=library.getItems().get(book) - 1;
-                                    library.getItems().put(book, borrowCopies);
+                                    library.getItems().replace(book, borrowCopies);
                                 } else {
                                     System.out.println("Sorry, the book is already issued.");
                                 }
@@ -54,7 +54,7 @@ import LibraryProject.*;
                                     System.out.println("The book has been returned.");
                                     book.setBookAvailability(true);
                                     int availableCopies = library.getItems().get(book) + 1;
-                                    library.getItems().put(book, availableCopies);
+                                    library.getItems().replace(book, availableCopies);
                                 }
                             }
                         break;
